60b9fa1cf45ca84713757836c917d14413535890
sorojasp <stivenorlandorojaspulido@gmail.com>
2021-03-25 23:00:58 -0500

    se empieza a reducir la información para guardar la mayor cantidad de datos en el ESP8266-01

--- 0.sourceCodes/pruebaTime/timePrueba/timePrueba.ino
+++ 0.sourceCodes/pruebaTime/timePrueba/timePrueba.ino
@@ -2,7 +2,7 @@
 
 #include <Time.h>
 #include <TimeLib.h>
- 
+#include "decToBin.h" 
 // Declaramos la variable del tipo time_t
 
  time_t fecha;
@@ -10,6 +10,10 @@ void setup() {
   Serial.begin(9600);
 
 }
+
+
+
+
  
 void loop() {
   
@@ -20,15 +24,98 @@ void loop() {
   fecha = now();
  
 
-  Serial.println(fecha);
+  //Serial.println(fecha);
   /*
    from datetime import datetime
    print(datetime.fromtimestamp(fecha))
    Para python, y se obtiene la fecha
   */
-  Serial.println(sizeof(fecha));
-  Serial.println(fecha,HEX);
-  byte n = 0b10011001;
-  byte probe = (byte) "0b11111111";
-  Serial.println(probe);
+  //Serial.println(sizeof(fecha));
+  //Serial.println(2000,HEX);
+  byte n = byte(1023);
+//String(decToByn10(456))
+//decToByn7(99);
+//decToByn10(186);
+  int a[7]={1,1,0,0,1,0,1};
+   Serial.println(sizeof(decToBin(byn7ToDec(a))));
+   Serial.println(byn7ToDec(a));
+
 }
+
+double datosTratados(int enteroPromedio, int decimasPromedio, int enteroDesviacion,  int decimasDesviacion ){
+
+  return 2.6;
+ 
+   
+}
+
+int decToByn10(int num ){
+  int cantidadBits=10;
+
+    int a[cantidadBits]={0,0,0,0,0,0,0,0,0,0};
+  int i;
+  
+  for(i=cantidadBits-1;num>0;i--){
+    a[i]=num%2;    
+    num=num/2;    
+    
+    }
+
+  for(i=0;i<cantidadBits;i++){
+    Serial.print(a[i]);
+    }
+    
+    Serial.println("");
+    return a;
+  
+  }
+
+  int decToByn7(int num ){
+
+    int cantidadBits=7;
+
+    int a[cantidadBits]={0,0,0,0,0,0,0,0,0,0};
+  int i;
+  
+  for(i=cantidadBits-1;num>0;i--){
+    a[i]=num%2;    
+    num=num/2;    
+    
+    }
+
+  for(i=0;i<cantidadBits;i++){
+    Serial.print(a[i]);
+    }
+    
+    Serial.println("");
+    return a;
+  
+  }
+
+
+  byte byn7ToDec(int* bynNum7 ){
+    int cantidadBits=7;
+    double decimal=0;
+    int i=6;
+    int expo=0;
+
+
+    while(i>=0){
+      decimal=decimal + (pow(2,expo) *  bynNum7[i]);
+      expo++;
+      i--;
+      }
+
+     
+      int intDecimal = (int)(decimal) +1;
+      
+    
+      Serial.println(decToBin(intDecimal));
+      return decToBin(intDecimal);
+    
+  }
+
+  
+  byte byn10ToDec(int bynNum10 ){
+      return 12;
+  }
