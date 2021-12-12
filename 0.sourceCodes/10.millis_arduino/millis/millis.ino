2e4cb3f676f2abb662fa26d7222cc06fd2a8a2e7
sorojasp <stivenorlandorojaspulido@gmail.com>
2021-12-12 12:17:25 -0500

    the function of delay woth millis works, but have a error margin between 10% and 20%

--- 0.sourceCodes/10.millis_arduino/millis/millis.ino
+++ 0.sourceCodes/10.millis_arduino/millis/millis.ino
@@ -1,23 +1,72 @@
-unsigned long previousTime =0;
-unsigned long currentTime=0;
-unsigned long lapseTime=0;
+ unsigned long previousTime =0;
+  unsigned long currentTime =0;
+  unsigned long timeLapse=0;
+#define led 8
+boolean state=true;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, state);
 }
 
-void loop() {
+
+
+void loop() { 
+  previousTime=millis();
+  unsigned long delayUL = 2500UL;
+  
+  while(!(delay_millis(delayUL)==true)){}
+  
+  currentTime=millis();
+  timeLapse=currentTime-previousTime;
+
+  unsigned long porcentaje= ((timeLapse-delayUL)*100UL)/delayUL;
+
+  Serial.write(("timeLapse: "+String(timeLapse)+"-"+"porcentaje de error:"+String(porcentaje)+"%"+"\n").c_str());
 
   
-previousTime = millis();
-currentTime=millis();
-lapseTime=currentTime-previousTime;
+  state=!state;
+  digitalWrite(led, state);
 
 
-
-Serial.write(String("\n").c_str());
-Serial.write(String(lapseTime).c_str());
-Serial.flush();
+  
 }
+
+
+boolean delay_millis(unsigned long delayInMillis){
+  /*
+   * Esta funcíon presenta un margen de error de +entre 20% para valores menores a 1000UL y de un 11% para valores 
+   * mayores a 10000UL
+  
+  */
+  unsigned long previousTime =0;
+  unsigned long currentTime =0;
+  unsigned long totalTime=0;
+  unsigned long timeLapse=0;
+  unsigned long error=2UL;
+  boolean continueInCicle=true;
+  delayInMillis=delayInMillis/error;
+  
+
+  while(continueInCicle){
+    previousTime=millis();
+     if (totalTime>= delayInMillis){
+      continueInCicle=false;
+      totalTime=0;
+      return true;
+      }
+
+       currentTime=millis();
+       timeLapse=currentTime-previousTime;
+       totalTime=totalTime+timeLapse;
+    
+    }
+    return false;
+  
+  
+  
+  } 
 
 void serial_flush_buffer(){
   Serial.available();
