4d822b7cd9c75de053ce6cca6b7128da784acd95
sorojasp <stivenorlandorojaspulido@gmail.com>
2022-01-03 14:24:22 -0500

    probe with folder number 4 and 8266_uart

--- 0.sourceCodes/0.finalPrograms/8266_uart/8266_uart.ino
+++ 0.sourceCodes/0.finalPrograms/8266_uart/8266_uart.ino
@@ -21,7 +21,11 @@ void setup() {
 
   Serial.begin(9600);
 
-  recieveJunk(2000);
+  boolean resultRecieveJunk=recieveJunk(2000);
+
+  if(resultRecieveJunk==true){
+
+    
   String dataRecieved=recieveData(2000);
 
   sendData(2,"Hi from ESP8266 =)",2000);
@@ -29,8 +33,6 @@ void setup() {
   delay(100);
 
    if(connectToWifiNetwork(String(ssid), String(password))){
-
-       concentracionesSensores cs = {280.3, 78.36, 74.74, 85.4, 41.85, 20.8, 96.63, "Dec-9-2021h10:02:18" };
        WiFiClient client;
        sendSensorData(String(servername), String(endPoint), 80, dataRecieved, client);
 
@@ -38,6 +40,11 @@ void setup() {
 
 
   ESP.deepSleep(0);
+    
+    }
+
+
+  
 
 
 
